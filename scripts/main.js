35893e4ec1514bf99a9394854bc7bd6da01c1ec6
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-07-13 18:55:33 +0530

    handing city change event and started with pagination

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,35 @@ const prevButton = document.getElementById("prev-page-btn");
 const nextButton = document.getElementById("next-page-btn");
 const pageNumberContainer = document.getElementById('page-number');
 const pageSizeSelector = document.getElementById('page-size');
+
+selectCity.addEventListener("change", (e) => handleCitySelect(e.target.value));
+searchBar.addEventListener("input", (e) => handleSearchInput(e.target.value.toUpperCase()));
+
+
+//handle city dropdown selection
+const handleCitySelect = (cityName) => {
+    //show loader and hide table
+    bankDataContainer.style.display = 'none';
+    loader.style.display = '';
+
+
+}
+//start thinking how to paginate
+
+let currPage;
+let pageSize = 250;
+let startIndex;
+let pagesCount;
+
+const previousPage = () => {
+
+}
+
+const nextPage = () => {
+
+}
+
+const paginate = () => {
+
+}
+ 
